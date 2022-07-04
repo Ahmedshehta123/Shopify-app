56e5faa34c3dec566a44d615059891fa75f1effe
ahmed shehta <shehta029@gmail.com>
2022-07-04 08:09:59 -0700

    number1

---
+++ finalProject/Home/Network manager/Decoder.swift
@@ -0,0 +1,14 @@
+//
+//  Decoder.swift
+//  finalProject
+//
+//  Created by Ahmed on 04/07/2022.
+//
+
+import Foundation
+func convertFromJson<T: Codable>(data : Data) -> T? {
+    print("decode data")
+    let jsonDecoder = JSONDecoder()
+    let decodeArray = try? jsonDecoder.decode(T.self, from: data)
+    return decodeArray
+}
